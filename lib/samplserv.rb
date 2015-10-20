3120bc7a1b232f18fe58ba17107770cc1c7ae35a
Harrison Andrews <harrisonandrews1986@gmail.com>
2015-10-20 16:58:12 -0400

    added all of the different sound clips

--- lib/samplserv.rb
+++ lib/samplserv.rb
@@ -16,6 +16,103 @@ module Samplserv
       spawn("afplay \"samples/Better#{version}.mp3\"")
     end
 
+    post "/beat" do
+      spawn("afplay \"samples/Beat.mp3\"")
+    end
+
+    post "/faster" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Faster#{version}.mp3\"")
+    end
+
+    post "/after" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/After#{version}.mp3\"")
+    end
+
+    post "/doit" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Doit#{version}.mp3\"")
+    end
+
+    post "/ever" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Ever#{version}.mp3\"")
+    end
+
+    post "/harder" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Harder#{version}.mp3\"")
+    end
+
+    post "/hour" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Hour#{version}.mp3\"")
+    end
+
+    post "/makeit" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Makeit#{version}.mp3\"")
+    end
+
+    post "/makeus" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Makeus#{version}.mp3\"")
+    end
+
+    post "/morethan" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Morethan#{version}.mp3\"")
+    end
+
+    post "/never" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Never#{version}.mp3\"")
+    end
+
+    post "/our" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Our#{version}.mp3\"")
+    end
+
+    post "/over" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Over#{version}.mp3\"")
+    end
+
+    post "/stronger" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Stronger#{version}.mp3\"")
+    end
+
+    post "/workis" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Workis#{version}.mp3\"")
+    end
+
+    post "/workit" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Workit#{version}.mp3\"")
+    end
+
+
+
+
     get "/stop" do
       spawn("killall afplay")
       "So much for the music..."
@@ -23,4 +120,5 @@ module Samplserv
 
     run! if app_file == $0
   end
+
 end
